8c656a7d6dec76f788bd6119483ffc8d9477b6f5
Noah Bathras <nbathras@terpmail.umd.edu>
2019-09-21 17:53:03 -0400

    Finished p2

--- projects/p2/flask_app/app.py
+++ projects/p2/flask_app/app.py
@@ -6,22 +6,34 @@ poke_client = PokeClient()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
 
-@app.route()
-def pokemon_info(name):
+    pokemon_list = poke_client.get_pokemon_list()
+    
+    return render_template('index.html', pokemon_list=pokemon_list)
+
+@app.route('/pokemon/<pokemon_name>')
+def pokemon_info(pokemon_name):
     """
     Must show all the info for a pokemon identified by name
 
     Check the README for more detail
     """
-    pass
+    
+    pokemon = poke_client.get_pokemon_info(pokemon_name)
+    
+    return render_template('pokemon_info.html', pokemon=pokemon)
 
-@app.route()
-def pokemon_with_ability(ability):
+@app.route('/ability/<ability_name>')
+def pokemon_with_ability(ability_name):
     """
     Must show a list of pokemon 
 
     Check the README for more detail
     """
-    pass
+    
+    context = dict()
+    
+    context["ability_name"] = ability_name
+    context["pokemon_list"] = pokemon_list = poke_client.get_pokemon_with_ability(ability_name)
+    
+    return render_template('pokemon_with_ability.html', context=context)
